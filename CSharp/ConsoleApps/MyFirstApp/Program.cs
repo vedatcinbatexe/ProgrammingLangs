b2cc6b80402309263ee2d49c74af68a0c4c910f1
Vedat Cinbat <cinbatvedat@gmail.com>
2025-11-27 21:24:35 +0300

    Foundations in C# done

--- CSharp/ConsoleApps/MyFirstApp/Program.cs
+++ CSharp/ConsoleApps/MyFirstApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello First App!");
+﻿using System;
+
+namespace MyFirstApp {}
+
+class Program
+{
+    // Method: The entry point. 'Main' is C# always starts
+    static void Main(string[] args)
+    {
+        // This is comment
+        Console.WriteLine("System Booting...");
+        Console.WriteLine("Welcome to the C# Architecture");
+    }
+}
